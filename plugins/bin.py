dc2e63224d691047d1320d63106c3996b13d4c28
ilhammansiz <58665444+ilhammansiz@users.noreply.github.com>
2021-07-10 09:00:35 +0000

    pylint: auto fixes

--- plugins/bin.py
+++ plugins/bin.py
@@ -1,4 +1,3 @@
-
 """
 💐 Commands Available : bin
 • `{i}bin`
@@ -7,95 +6,113 @@
 
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
+
 from . import *
+
 
 @ilhammansiz_cmd(pattern="bin ?(.*)")
 async def _(event):
     if event.fwd_from:
-        return 
-    
+        return
+
     danish = event.pattern_match.group(1)
     chat = "@Carol5_bot"
     await event.edit("mencari bin untuk anda 😁...")
     async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1247032902))
-              await event.client.send_message(chat, "/bin {}".format(danish))
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("sensei!, tolong unblock @Carol5_bot ")
-              return
-          if response.text.startswith(" "):
-             await event.edit("maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1247032902)
+            )
+            await event.client.send_message(chat, "/bin {}".format(danish))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("sensei!, tolong unblock @Carol5_bot ")
+            return
+        if response.text.startswith(" "):
+            await event.edit(
+                "maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔"
+            )
+        else:
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
 
 
 @ilhammansiz_cmd(pattern="vbv ?(.*)")
 async def _(event):
     if event.fwd_from:
-        return 
-    
+        return
+
     danish = event.pattern_match.group(1)
     chat = "@Carol5_bot"
     await event.edit("menghubungkan...")
     async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1247032902))
-              await event.client.send_message(chat, "/vbv {}".format(danish))
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("sensei!, tolong unblock @Carol5_bot")
-              return
-          if response.text.startswith(" "):
-             await event.edit("maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1247032902)
+            )
+            await event.client.send_message(chat, "/vbv {}".format(danish))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("sensei!, tolong unblock @Carol5_bot")
+            return
+        if response.text.startswith(" "):
+            await event.edit(
+                "maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔"
+            )
+        else:
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
+
 
 @ilhammansiz_cmd(pattern="key ?(.*)")
 async def _(event):
     if event.fwd_from:
-        return 
-    
+        return
+
     danish = event.pattern_match.group(1)
     chat = "@Carol5_bot"
     await event.edit("menghubungkan...")
     async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1247032902))
-              await event.client.send_message(chat, "/key {}".format(danish))
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("sensei!, tolong unblock @Carol5_bot ")
-              return
-          if response.text.startswith(" "):
-             await event.edit("maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1247032902)
+            )
+            await event.client.send_message(chat, "/key {}".format(danish))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("sensei!, tolong unblock @Carol5_bot ")
+            return
+        if response.text.startswith(" "):
+            await event.edit(
+                "maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔"
+            )
+        else:
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
 
-  
+
 @ilhammansiz_cmd(pattern="iban ?(.*)")
 async def _(event):
     if event.fwd_from:
-        return 
-    
+        return
+
     danish = event.pattern_match.group(1)
     chat = "@Carol5_bot"
     await event.edit("menghubungkan...")
     async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1247032902))
-              await event.client.send_message(chat, "/iban {}".format(danish))
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("sensei!, tolong unblock @Carol5_bot ")
-              return
-          if response.text.startswith(" "):
-             await event.edit("maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1247032902)
+            )
+            await event.client.send_message(chat, "/iban {}".format(danish))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("sensei!, tolong unblock @Carol5_bot ")
+            return
+        if response.text.startswith(" "):
+            await event.edit(
+                "maaf sensei, ternyata bot nya telah tewas dan tidak berfungsi lagi 😔"
+            )
+        else:
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
