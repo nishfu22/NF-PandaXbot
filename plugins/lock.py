b1e7038a3cf16b5cf46303fdb60eecfff3d37e98
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-10 15:51:58 +0700

    Create lock.py

---
+++ plugins/lock.py
@@ -0,0 +1,38 @@
+"""
+💐 Commands Available -
+• `{i}lock <msgs/media/sticker/gif/games/inline/polls/invites/pin/changeinfo>`
+    Lock the Used Setting in Used Group.
+• `{i}unlock <msgs/media/sticker/gif/games/inline/polls/invites/pin/changeinfo>`
+    UNLOCK the Used Setting in Used Group.
+"""
+
+from PandaX_Userbot.functions.all import lucks, unlucks
+from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
+
+from . import *
+
+
+@ilhammansiz_cmd(pattern="lock ?(.*)", groups_only=True, admins_only=True)
+async def lockho(e):
+    mat = e.pattern_match.group(1)
+    if not mat:
+        return await eod(e, "`What to Lock  ?`")
+    try:
+        ml = lucks(mat)
+    except BaseException:
+        return await eod(e, "`Incorrect Input`")
+    await petercordpanda_bot(EditChatDefaultBannedRightsRequest(e.chat_id, ml))
+    await eor(e, f"Locked - `{mat}` ! ")
+
+
+@ilhammansiz_cmd(pattern="unlock ?(.*)", groups_only=True, admins_only=True)
+async def unlckho(e):
+    mat = e.pattern_match.group(1)
+    if not mat:
+        return await eod(e, "`What to Lock  ?`")
+    try:
+        ml = unlucks(mat)
+    except BaseException:
+        return await eod(e, "`Incorrect Input`")
+    await petercordpanda_bot(EditChatDefaultBannedRightsRequest(e.chat_id, ml))
+    await eor(e, f"Unlocked - `{mat}` ! ")
